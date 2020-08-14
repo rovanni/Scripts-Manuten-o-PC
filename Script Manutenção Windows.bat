1764e7322ed8e99ee71ba3506c15a95ce014b467
Luciano <rovanni@users.noreply.github.com>
2020-08-14 09:32:27 -0300

    Adicionado a opção de esvaziar a pasta

--- Script Manutenção Windows.bat
+++ Script Manutenção Windows.bat
@@ -13,10 +13,11 @@ echo ========================================================================
 echo * 1. Esvaziar Arq. Temp.                                               * 
 echo * 2. Esvaziar Arq. Temp. Completa                                      *  
 echo * 3. Esvaziar Spooler de Impressão.                                    *
-echo * 4. Verificador o Windows e reparar arquivos ausentes ou corrompidos  *
-echo * 5. Reiniciar                                                         *
-echo * 6. Desligar                                                          *
-echo * 7. Sair                                                              * 
+echo * 4. Esvaziar Pasta Downloads                                          *
+echo * 5. Verificador o Windows e reparar arquivos ausentes ou corrompidos  *
+echo * 6. Reiniciar                                                         *
+echo * 7. Desligar                                                          *
+echo * 8. Sair                                                              * 
 echo ========================================================================
 
 set /p opcao= Escolha uma opção: 
@@ -27,7 +28,9 @@ if %opcao% equ 3 goto opcao3
 if %opcao% equ 4 goto opcao4
 if %opcao% equ 5 goto opcao5
 if %opcao% equ 6 goto opcao6
-if %opcao% GEQ 7 goto opcao7
+if %opcao% equ 7 goto opcao7
+if %opcao% equ 8 goto opcao8
+if %opcao% GEQ 9 goto opcao9
 
 :opcao1
 cls
@@ -48,19 +51,17 @@ goto menu
 cls
 IF EXIST c:\windows\temp\ DEL /f /s /q c:\windows\temp\
 DEL /q /f /s  %temp%\
+del /s /f /q %temp%\*.*
 DEL /q /f /s  "%temp%"
-DEL /q /f /s C:\Documents and settings\%username%\Config~1\tempor~1
-DEL /q /f /s C:\Documents and settings\%username%\Config~1\temp
-DEL /q /f /s "%temp%"
-DEL /q /f /s C:\Windows\Prefetch
-RD /q /s  %systemdrive%\$Recycle.bin
-
 del /s /f /q c:\windows\temp\*.*
 rd /s /q c:\windows\temp
 md c:\windows\temp
-del /s /f /q %temp%\*.*
 rd /s /q %temp%
 md %temp%
+DEL /q /f /s C:\Documents and settings\%username%\Config~1\tempor~1
+DEL /q /f /s C:\Documents and settings\%username%\Config~1\temp
+DEL /q /f /s C:\Windows\Prefetch
+RD /q /s  %systemdrive%\$Recycle.bin
 cls
 echo ==================================
 echo *      Arq. Temp. Esvaziados      *
@@ -83,6 +84,16 @@ goto menu
 
 :opcao4
 cls
+DEL /q /f /s C:\Documents and settings\%username%\Downloads\*.*
+echo ==================================
+echo *   Pasta Downloads Esvaziada    *
+echo ==================================
+timeout /t 5
+goto menu
+
+
+:opcao5
+cls
 sfc /scannow
 
 Dism /Online /Cleanup-Image /ScanHealth
@@ -94,23 +105,23 @@ echo ==================================
 timeout /t 5
 goto menu
 
-:opcao5
+:opcao6
 cls
 shutdown /r /t 0 /c "Reinicio Programado" /f /d p:4:1 
 timeout /t 5
 goto menu
 
-:opcao6
+:opcao7
 cls
 shutdown /s /t 0 /c "Desligamento Programado" /f /d p:4:1 
 timeout /t 5
 goto menu
 
-:opcao7
+:opcao8
 cls
 exit
 
-:opcao8
+:opcao9
 echo ==============================================
 echo * Opcao Invalida! Escolha outra opcao do menu *
 echo ==============================================
